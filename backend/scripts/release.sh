76be5a85ac2c39d3dccdc28e6baebf45d07a25c8
sol <solanshmontoyamm@gmail.com>
2021-10-19 21:23:22 -0500

    loaddata deleted for deploy

--- backend/scripts/release.sh
+++ backend/scripts/release.sh
@@ -6,7 +6,6 @@ python manage.py migrate --no-input
 if [[ $ENVIRONMENT == "stage" ]]
 then
 	python manage.py collectstatic --no-input -i admin -i s3direct -i *node_mod1ules* -i *bower_components* -i debug_toolbar
-    python manage.py loaddata fixtures/initial-data
 fi
 
 # to restore prod database on stage

ba7454bc001f1568b57f11075d047f30ba847e4c
sol <solanshmontoyamm@gmail.com>
2022-03-30 14:25:02 -0500

    new domain for jaku added

--- backend/AppWeb/AppWeb/settings/heroku.py
+++ backend/AppWeb/AppWeb/settings/heroku.py
@@ -13,7 +13,7 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 
-ALLOWED_HOSTS = ['.herokuapp.com', '.appweb.net']
+ALLOWED_HOSTS = ['.herokuapp.com', '.appweb.net', 'jaku.edu.pe']
 
 MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware'
